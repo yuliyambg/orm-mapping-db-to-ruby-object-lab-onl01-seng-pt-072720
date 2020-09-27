eb67e037983f867d8f03640a499670613f4dc8d6
Yuliya Marinova <yuliyambg@gmail.com>
2020-09-27 22:28:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,13 +37,15 @@ class Student
   end
   
   
-  def all_students_in_grade_9(grade)
+  def self.all_students_in_grade_9
     sql = <<-SQL
-    SELECT *FROM students
-    WHERE grade = ?
+    SELECT COUNT(grade) 
+    FROM students
+    WHERE grade = 9
     SQL
-    DB[:conn].execute(sql, grade)
-    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   
